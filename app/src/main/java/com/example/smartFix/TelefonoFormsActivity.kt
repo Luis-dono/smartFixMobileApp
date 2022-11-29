b952206928f4e79e4c2dbca34e867da48e0c85d0
inkicidor <inkicidor91@hotmail.com>
2022-11-28 23:12:51 -0700

    get retrofit detalles

--- app/src/main/java/com/example/smartFix/TelefonoFormsActivity.kt
+++ app/src/main/java/com/example/smartFix/TelefonoFormsActivity.kt
@@ -5,47 +5,51 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.EditText
+import android.widget.Toast
 import com.example.smartFix.apiRetrofit.SfApi
+import com.example.smartFix.models.DetalleResponse
 import com.example.smartFix.models.Resultado
 import kotlinx.coroutines.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.awaitResponse
 
 class TelefonoFormsActivity : AppCompatActivity(), OnClickListener {
-    private val detalle= mutableListOf<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_telefono_forms)
-        searchByFolio()
+        var folio: String? = intent.extras?.getString("folio")
+        if (folio != null) {
+            merequetengue(folio)
+        };
     }
+    private fun merequetengue(folio : String){
+       println(folio);
+        var call:Call<DetalleResponse> =SfApi.instance.getDetalle(folio+"/")
+        call.enqueue(object : Callback<DetalleResponse>{
+            override fun onResponse(call: Call<DetalleResponse>, response:Response<DetalleResponse>){
+                if(response.isSuccessful){
+                    var dataDetalle: DetalleResponse = response.body()!!
+                    println("marca telefono")
+                    println(dataDetalle.toString())
+                }
+            }
 
+            override fun onFailure(call: Call<DetalleResponse>, t: Throwable) {
+                print("pincheperraputamadre fallo completamente")
+
+            }
+        })
+    }
     override fun onClick(p0: View?) {
 
     }
 
 
-    private fun searchByFolio() {
-        val apiinstance=SfApi.instance
-        GlobalScope.launch (Dispatchers.IO){
-            val response=apiinstance.getDetalle().awaitResponse()
-            if(response.isSuccessful){
-                val data: Resultado? =response.body()
-                withContext(Dispatchers.Main){
-                    val editText=findViewById<EditText>(R.id.editTextmarca)
 
-                    if (data != null) {
-                        editText.setText(data.marca)
-                        println("el modelo del telefono es--"+data.modelo)
-                    }
-                }
-            }else{
-                println("no jala esta mrda")
-            }
-        }
-    }
-     private fun addInfoForms(){
-         val editText=findViewById<EditText>(R.id.editTextmarca)
-        editText.setText(detalle[1])
-     }
 }
 //1c798653
+//e798e01f
--- app/src/main/java/com/example/smartFix/apiRetrofit/SfApi.kt
+++ app/src/main/java/com/example/smartFix/apiRetrofit/SfApi.kt
@@ -1,5 +1,6 @@
 package com.example.smartFix.apiRetrofit
 
+import com.example.smartFix.models.DetalleResponse
 import com.example.smartFix.models.Resultado
 import okhttp3.OkHttpClient
 import retrofit2.Call
@@ -7,16 +8,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Url
 
 interface SfApi {
 
     companion object{
-        val instance = Retrofit.Builder().baseUrl("https://api-smartfixing.auplex.mx/").addConverterFactory(GsonConverterFactory.create()).client(
+        val instance = Retrofit.Builder().baseUrl("https://api-smartfixing.auplex.mx/telefono/detalle/").addConverterFactory(GsonConverterFactory.create()).client(
             OkHttpClient().newBuilder().build()
         ).build().create(SfApi::class.java)
     }
 
-@GET("telefono/detalle/{folio}")
-fun getDetalle(@Path("folio") folio:String): Call<Resultado>
+@GET
+fun getDetalle(@Url url:String): Call<DetalleResponse>
 
 }
