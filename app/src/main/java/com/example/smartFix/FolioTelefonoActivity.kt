b2642bec2db925340cac95a4d190a6fc4049a8d3
ElOsuna <luisenrique81620@gmail.com>
2022-12-03 20:20:01 -0700

    Fix asignar tecnico y getdetalle api

--- app/src/main/java/com/example/smartFix/FolioTelefonoActivity.kt
+++ app/src/main/java/com/example/smartFix/FolioTelefonoActivity.kt
@@ -36,13 +36,53 @@ class FolioTelefonoActivity : AppCompatActivity() {
        val tecnicoid: Int? = bundle?.getInt("tecnicoId")
        var folioTxt = folioET.text.toString().trim()
 
-       folioProceso(folioTxt,tecnicoid!!)
+       getDetalleTelefono(folioTxt,tecnicoid!!)
 
-       }
+   }
 
-    private fun folioProceso(folio:String, tecnicoid:Int){
+    private fun getDetalleTelefono(folio:String,tecnicoid:Int) {
+
+        var call: Call<TelefonData> = SfApi.instance.getDetalleTelefonoByFolio(folio)
+        call.enqueue(object : Callback<TelefonData?> {
+            override fun onResponse(call: Call<TelefonData?>, response: Response<TelefonData?>) {
+                if (response.isSuccessful) {
+                    Log.d("Garaje", "RESPONSE SUCCESFULL")
+                    var detalleTelefonoData: TelefonData = response.body()!!
+                    var detalleActual: com.example.smartFix.apiRetrofit.models.Detalle = detalleTelefonoData.resultados.get(0)
+                    val tecid: Int?
+                    tecid = detalleActual.tecnicoid
+                    Log.d("Garaje", "tec id $tecid")
+                    if (tecid == 0) {
+                        folioProceso(folio, tecnicoid,2)
+                        envioDatos(detalleActual)
+                    }else if (tecid != tecnicoid){
+                        Toast.makeText(applicationContext,"El teléfono ya se encuentra asignado a otro técnico", Toast.LENGTH_LONG).show()
+                        return
+                    }
+                    Log.d("Garaje", "ENTRE AL GET EXITOSAMENTE ${detalleTelefonoData.resultados}")
+                    envioDatos(detalleActual)
+                }
+            }
+
+            override fun onFailure(call: Call<TelefonData?>, t: Throwable) {
+                Log.d("Garaje", "${t.toString()}")
+            }
+        })
+    }
+
+    fun envioDatos(detalle: Detalle){
+        val intent = Intent(this@FolioTelefonoActivity,TelefonoFormsActivity::class.java)
+        intent.putExtra("marca",detalle.marca)
+        intent.putExtra("modelo",detalle.modelo)
+        intent.putExtra("descripcion",detalle.descripcion_ingreso)
+        intent.putExtra("color",detalle.color)
+        intent.putExtra("estatus",detalle.estatusid)
+        startActivity(intent)
+    }
+
+    private fun folioProceso(folio:String, tecnicoid:Int,estatusid:Int){
         Log.d("Garaje","Entre a folio $tecnicoid $folio")
-        val data = PatchData(2,tecnicoid)
+        val data = PatchData(estatusid,tecnicoid)
         var call: Call<AsignacionTecnicoResponse> = SfApi.instance.asignarTecnico(folio, data)
         call.enqueue(object: Callback<AsignacionTecnicoResponse?>{
             override fun onResponse(call: Call<AsignacionTecnicoResponse?>, response: Response<AsignacionTecnicoResponse?>) {
@@ -51,11 +91,6 @@ class FolioTelefonoActivity : AppCompatActivity() {
                     print(dataFolio.tecnicoid)
                     print(dataFolio.folio)
                     Log.d("Garage","DATO ERROR ${dataFolio.error}")
-                    if (!dataFolio.error!!){
-                        getDetalleTelefono(folio)
-                    }
-                }else{
-                    Toast.makeText(applicationContext,"El teléfono ya se encuentra asignado a otro técnico", Toast.LENGTH_LONG).show()
                 }
             }
 
@@ -67,34 +102,8 @@ class FolioTelefonoActivity : AppCompatActivity() {
         })
     }
 
-    private fun getDetalleTelefono(folio:String){
-
-        var call: Call<TelefonData> = SfApi.instance.getDetalleTelefonoByFolio(folio)
-        call.enqueue(object: Callback<TelefonData?>{
-            override fun onResponse(call: Call<TelefonData?>, response: Response<TelefonData?>) {
-                if (response.isSuccessful){
-                    var detalleTelefonoData: TelefonData = response.body()!!
-                    var detalleActual:com.example.smartFix.apiRetrofit.models.Detalle=detalleTelefonoData.resultados.get(0)
-                    envioDatos(detalleActual)
-                    Log.d("Garaje","ENTRE AL GET EXITOSAMENTE ${detalleTelefonoData.resultados}")
-                   // Log.d("Garaje","ENTRE AL GET EXITOSAMENTE ${detalleTelefonoData.resultados.get(0).apellido}")
-                }
-            }
-            fun envioDatos(detalle: Detalle){
-                val intent = Intent(this@FolioTelefonoActivity,TelefonoFormsActivity::class.java)
-                intent.putExtra("marca",detalle.marca)
-                intent.putExtra("modelo",detalle.modelo)
-                intent.putExtra("descripcion",detalle.descripcion_ingreso)
-                intent.putExtra("color",detalle.color)
-                startActivity(intent)
-            }
-            override fun onFailure(call: Call<TelefonData?>, t: Throwable) {
-                Log.d("Garaje","${t.toString()}")
-            }
 
 
-        })
-    }
 
 }
 
