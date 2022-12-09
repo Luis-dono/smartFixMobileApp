9bef97e96fd2e09afe730098692d071a38357945
ElOsuna <luisenrique81620@gmail.com>
2022-12-08 22:31:07 -0700

    Borrado de logs.d y clases no usadas

--- app/src/main/java/com/example/smartFix/apiRetrofit/models/DataLogin.kt
+++
@@ -1,8 +0,0 @@
-package com.example.smartFix.apiRetrofit.models
-
-import com.google.gson.annotations.SerializedName
-
-data class DataLogin(
-    @SerializedName("usuario") val email: String,
-    @SerializedName("password") val password: String
-    )
